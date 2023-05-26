99f8788969b4672dbd18faf00c8ed6a480218bc6
Gaurav Sharma <23628920+gaurav-js-dev@users.noreply.github.com>
2023-05-26 09:34:45 +0530

    Added framer motion demo poc code

---
+++ framer-motion-demo/src/Example.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { shuffle } from "lodash";
+
+const spring = {
+  type: "spring",
+  damping: 20,
+  stiffness: 300
+};
+
+export const Example = () => {
+  const [colors, setColors] = useState(initialColors);
+
+  useEffect(() => {
+    setTimeout(() => setColors(shuffle(colors)), 1000);
+  }, [colors]);
+
+  return (
+    <ul>
+      {colors.map((background) => (
+        <motion.li
+          key={background}
+          layout
+          transition={spring}
+          style={{ background }}
+        />
+      ))}
+    </ul>
+  );
+};
+
+const initialColors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF"];
